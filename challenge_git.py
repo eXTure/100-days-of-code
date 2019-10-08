a3e0245a75bc0673c9d4731a851ab7269f0e7955
Tomas Ribinskis <tomas.ribinskis@gmail.com>
2019-10-08 23:34:44 +0300

    Challenge progress update

--- challenge_git.py
+++ challenge_git.py
@@ -1,6 +1,8 @@
 #Script to automate #100daysofcode challenge pushing to github
 import subprocess as sp
 
+sp.run('git status')
 sp.run('git add .')
 sp.run('git commit -m "Challenge progress update"')
 sp.run('git push')
+sp.run('git status')
